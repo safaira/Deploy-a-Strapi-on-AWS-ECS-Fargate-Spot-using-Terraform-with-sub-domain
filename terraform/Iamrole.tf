2de826caf670222f08e247bcb0090dae2d7747a9
saniya <sani2mansuri@gmail.com>
2024-07-07 23:08:45 +0530

    iam role for ssm to decrypt secrets

--- terraform/Iamrole.tf
+++ terraform/Iamrole.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
 
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
@@ -10,15 +15,10 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
-
-  inline_policy {
-    name = "ecs-ssm-permission"
-
-    policy = jsonencode({
+resource "aws_iam_policy" "ecs_secret_permission" {
+  name        = "ecs-secret-permission"
+  description = "Policy to allow ECS tasks to access Docker Hub credentials"
+  policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
@@ -28,8 +28,8 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
           "kms:Decrypt"
         ],
         Resource = [
-          "arn:aws:ssm:ap-south-1:687157172064:parameter/DOCKERHUB_PASSWORD",
-          "arn:aws:ssm:ap-south-1:687157172064:parameter/DOCKERHUB_USERNAME",
+          "arn:aws:secretsmanager:ap-south-1:687157172064:secret:DOCKERHUB_PASSWORD",
+          "arn:aws:secretsmanager:ap-south-1:687157172064:secret:DOCKERHUB_USERNAME",
           "arn:aws:kms:ap-south-1:687157172064:key/4806d5f3-2615-4ddf-a3a6-c36c9bf72570"
         ]
       }
@@ -41,4 +41,8 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
                 
+}
+resource "aws_iam_role_policy_attachment" "ecs_secret_permission_attachment" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "aws_iam_policy.ecs_secret_permission.arn"
 }
