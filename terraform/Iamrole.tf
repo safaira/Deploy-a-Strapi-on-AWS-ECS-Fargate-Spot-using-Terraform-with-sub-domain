da11c405fc49821ba7b5580968ba66b6b68268b7
saniya <sani2mansuri@gmail.com>
2024-07-07 23:06:12 +0530

    iam role for ssm to decrypt secrets

--- terraform/Iamrole.tf
+++ terraform/Iamrole.tf
@@ -1,4 +1,15 @@
 
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
@@ -25,42 +36,6 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
     ]
   })
 }
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-
-
-
-# resource "aws_iam_policy" "ecs_secret_permission" {
-#   name        = "ecs-secret-permission"
-#   description = "Policy to allow ECS tasks to access Docker Hub credentials"
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "secretsmanager:GetSecretValue",
-#           "kms:Decrypt"
-#         ],
-#         Resource = [
-#           "arn:aws:ssm:ap-south-1:687157172064:parameter/DOCKERHUB_PASSWORD",
-#           "arn:aws:ssm:ap-south-1:687157172064:parameter/DOCKERHUB_USERNAME",
-#           "arn:aws:kms:ap-south-1:687157172064:key/4806d5f3-2615-4ddf-a3a6-c36c9bf72570"
-#         ]
-#       }
-#     ]
-#   })
-# }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
